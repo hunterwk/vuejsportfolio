e50ce451d9dbbe8879d507712366fd31f62f17a3
Hunter MM <hunterkantner@gmail.com>
2021-04-15 17:11:59 -0700

    added about component

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,18 +1,34 @@
 <template>
-  <p>
-    I am full stack web developer born and raised in San Diego. I enjoy building
-    components and learning more and more about modern frameworks. I recently
-    graduated a full time bootcamp via UCSD that focused on HTML, CSS,
-    Javascript, MYSQL, Nodejs and React. It was a difficult and rewarding
-    challenge but it helped me learn how to code in a group setting and
-    understand sprint deadlines.
-  </p>
+  <div>
+    <p>
+      I am full stack web developer born and raised in San Diego. I enjoy
+      building components and learning more and more about modern frameworks. I
+      recently graduated a full time bootcamp via UCSD that focused on HTML,
+      CSS, Javascript, MYSQL, Nodejs and React. It was a difficult and rewarding
+      challenge but it helped me learn how to code in a group setting and
+      understand sprint deadlines.
+    </p>
+    <a
+      href="../assets/resume.pdf"
+      target="_blank"
+      ><img class="icon" src="../assets/cv.svg" /><br /><span
+        >My Resume</span
+      ></a
+    >
+  </div>
 </template>
 <script>
 export default {
   name: "About",
-  
 };
 </script>
 <style scoped>
+p {
+  margin: 16px 100px 0px 100px;
+}
+.icon {
+  height: 60px;
+  width: 60px;
+  margin: 10px 10px 10px;
+}
 </style>
